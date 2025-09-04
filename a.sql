9fde1b679edd3ec0cc8b3af4e83a893d698b8d4a
John Swanson <john.d.swanson@gmail.com>
2025-09-04 07:07:19 -0700

    initial commit

---
+++ a.sql
@@ -0,0 +1,12 @@
+-- METABASE_BEGIN
+-- entity: "model/Transform:v1"
+-- identifier: my-transform
+-- name: the coolest transform
+-- target:
+--   type: "table"
+--   name: "meowmix"
+-- description: "a description"
+-- database: "AppDB"
+-- METABASE_END
+
+SELECT 123;
